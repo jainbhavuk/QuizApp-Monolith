60216c3bf4d0a1d043f54ed19441623b67102eca
jainbhavuk <bhavukspeaks@gmail.com>
2023-10-29 15:38:23 +0530

    Quiz App Monolith Architecture

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -11,6 +11,6 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
 
-    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
